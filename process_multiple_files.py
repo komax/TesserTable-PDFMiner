294793fba0f2956d2dc7d38fb8b29a4eea1ecc53
Maximilian Konzack <maximilian.konzack@idiv.de>
2018-09-04 17:46:34 +0200

    Parallelize the processing of the papers

--- process_multiple_files.py
+++ process_multiple_files.py
@@ -5,6 +5,7 @@ import sys
 
 from pathlib2 import Path
 from subprocess import call
+from multiprocessing import Pool
 
 from table_extractor import extract_tables
 
@@ -22,7 +23,8 @@ def select_pdfs(input_dir):
 
 def file_name_without_ext(path):
     file_name = path.resolve().stem
-    return file_name.replace(" ", "_").replace("'", "").replace(",", "")
+    return file_name.replace(" ", "_").replace("'", "").replace(",", "")\
+        .replace("(", "").replace(")", "")
 
 
 def process_publication(pdf_path, out_dir):
@@ -35,22 +37,40 @@ def process_publication(pdf_path, out_dir):
     # print "Completed table extraction {} ...".format(pdf_path)
 
 
+def create_output_directory(pdf_path):
+    file_name = file_name_without_ext(pdf_path)
+    out_directory = Path(OUT_DIR) / file_name
+    out_directory.mkdir(parents=True, exist_ok=True)
+    return out_directory
+
+
+def handle_paper(pdf_path):
+    out_directory = create_output_directory(pdf_path)
+    process_publication(str(pdf_path), str(out_directory))
+
+
+OUT_DIR = "./output"
+
+
 def main():
     parser = set_up_argparser()
     args = parser.parse_args()
 
-    out_dir = "./output"
+    global OUT_DIR
     if args.output_directory:
-        out_dir = args.output_directory
+        OUT_DIR = args.output_directory
 
     pdfs = select_pdfs(args.inputdir)
 
-    for pdf_path in pdfs:
-        file_name = file_name_without_ext(pdf_path)
-        out_directory = Path(out_dir) / file_name
-        out_directory.mkdir(parents=True, exist_ok=True)
+    p = Pool(4)
+    p.map(handle_paper, pdfs)
 
-        process_publication(str(pdf_path), str(out_directory))
+    # for pdf_path in pdfs:
+    #     file_name = file_name_without_ext(pdf_path)
+    #     out_directory = Path(out_dir) / file_name
+    #     out_directory.mkdir(parents=True, exist_ok=True)
+    #
+    #     process_publication(str(pdf_path), str(out_directory))
 
     #call(['./preprocess.sh', 'output/henry_et_al._2007', '/Users/mk21womu/Dropbox/Habitat loss meta-analysis/good_datasets/references/henry et al. 2007.pdf'])
     #extract_tables('output/henry_et_al._2007')
