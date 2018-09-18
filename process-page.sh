e5cf629e5e5d65d4576a9d163a46c105d750a66f
Maximilian Konzack <maximilian.konzack@idiv.de>
2018-09-18 16:01:05 +0200

    Renaming of this shell script to be more descriptiive

--- process-page.sh
+++ process-page.sh
@@ -4,4 +4,4 @@ tesseract $1/png/page_$2.png $1/tesseract/page_$2.html hocr txt
 mv $1/tesseract/page_$2.html.txt $1/tesseract-txt/page_$2.txt
 #tesseract -c hocr_font_info=1 $1/png/page_$2.png $1/tesseract/page_$2.html hocr
 mv $1/tesseract/page_$2.html.hocr $1/tesseract/page_$2.html
-python annotate.py $1/tesseract/page_$2.html $1/ocr_annotations/page_$2.png
+python ./annotate.py $1/tesseract/page_$2.html $1/ocr_annotations/page_$2.png
