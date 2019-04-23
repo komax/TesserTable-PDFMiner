ded37252337c8b6b8e75d1eedbd177f942c7939f
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-04-23 15:31:41 +0200

    Copying txt files to another folder

--- scripts/ocr_tesseract.sh
+++ scripts/ocr_tesseract.sh
@@ -5,4 +5,9 @@ HOCR_PATH=$2
 # HOCR_PATH without filetype extension.
 OUTBASE=${2%%.*}
 echo $OUTBASE
+# Call tesseract to generate hocr and txt file
 tesseract $PNG_PATH $OUTBASE hocr txt
+
+BASE_NAME=$(basename $HOCR_PATH .hocr)
+PAPER_PATH=$3
+mv $OUTBASE.txt $PAPER_PATH/ocr-txt/$BASE_NAME.txt
