bb827f2c1ad2a28a1258ef58b9b6e08a7f74612a
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-04-24 15:50:48 +0200

    Reworked ocr script to consider paths containing . in their name

--- scripts/ocr_tesseract.sh
+++ scripts/ocr_tesseract.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 PNG_PATH=$1
+#echo $PNG_PATH
 HOCR_PATH=$2
-# HOCR_PATH without filetype extension.
-OUTBASE=${2%%.*}
-echo $OUTBASE
-# Call tesseract to generate hocr and txt file
-tesseract $PNG_PATH $OUTBASE hocr txt
-
+#echo $HOCR_PATH
+# PATH_WITHOUT_EXT without filetype extension.
+OUTBASE=$(dirname $HOCR_PATH)
 BASE_NAME=$(basename $HOCR_PATH .hocr)
+PATH_WITHOUT_EXT="$OUTBASE/$BASE_NAME"
+#echo $OUTBASE
+echo "tesseract $PNG_PATH $PATH_WITHOUT_EXT hocr txt"
+# Call tesseract to generate hocr and txt file
+tesseract $PNG_PATH $PATH_WITHOUT_EXT hocr txt
+
 PAPER_PATH=$3
-mv $OUTBASE.txt $PAPER_PATH/ocr-txt/$BASE_NAME.txt
+mv $PATH_WITHOUT_EXT.txt $PAPER_PATH/ocr-txt/$BASE_NAME.txt
+echo "mv $PATH_WITHOUT_EXT.txt $PAPER_PATH/ocr-txt/$BASE_NAME.txt"
