775ddb3f9f78d1f43019988fdd98354fcba66211
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-04-29 19:46:48 +0200

    use one thread in tesseract

--- scripts/ocr_page.sh
+++ scripts/ocr_page.sh
@@ -10,7 +10,7 @@ PATH_WITHOUT_EXT="$OUTBASE/$BASE_NAME"
 # Call tesseract to generate hocr and txt file
 # TODO Describe https://github.com/tesseract-ocr/tesseract/issues/2312
 # OMP_THREAD_LIMIT=1 allows only one thread to be used in tesseract
-OMP_THREAD_LIMIT=2 tesseract $PNG_PATH $PATH_WITHOUT_EXT hocr txt
+OMP_THREAD_LIMIT=1 tesseract $PNG_PATH $PATH_WITHOUT_EXT hocr txt
 
 PAPER_PATH=$3
 mv $PATH_WITHOUT_EXT.txt $PAPER_PATH/ocr-txt/$BASE_NAME.txt
